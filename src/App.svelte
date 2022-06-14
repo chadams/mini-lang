7f155d27d20a750996f82c7320ddda4b779710da
Chad Adams <chad@variantllc.com>
2022-06-13 23:02:30 -0400

    SPA for github fix

--- src/App.svelte
+++ src/App.svelte
@@ -80,7 +80,11 @@
     <div class="sm:hidden hidden" id="mobile-menu">
       <div class="pt-2 pb-3 space-y-1">
         <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" -->
-        <a href="#" class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium" aria-current="page">
+        <a
+          href={import.meta.env.BASE_URL}
+          class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+          aria-current="page"
+        >
           Lessons
         </a>
       </div>
